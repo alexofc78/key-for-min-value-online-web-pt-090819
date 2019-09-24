588f3ceeab752467cc23802de335e5ea0c9b1674
ALEXANDRA OLIVEROS <alexofc@gmail.com>
2019-09-24 01:43:41 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -4,3 +4,17 @@
 def key_for_min_value(name_hash)
 
 end
+
+def key_for_min_value(hash)
+  smallest_key = nil
+  tiniest_value = nil
+  hash.each do |key, value|
+    if tiniest_value == nil || value < tiniest_value
+      tiniest_value = value
+      smallest_key = key
+    end
+  end
+  smallest_key
+end
+
+key_for_min_value(ikea)
