d0505b85b685318484a63db39bfe901f46d4e42c
ALEXANDRA OLIVEROS <alexofc@gmail.com>
2019-09-24 02:14:43 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,13 +1,13 @@
 # prereqs: iterators, hashes, conditional logic
 # Given a hash with numeric values, return the key for the smallest value
 
-def key_for_min_value(hash)
-  smallest_key = key
-  smallest_value = value
-  hash.each  do |key, value|
-    if value < smallest_value
-
-end
+#def key_for_min_value(hash)
+#  smallest_key = key
+#  smallest_value = value
+#  hash.each  do |key, value|
+#    if value < smallest_value
+#
+#end
 
 def key_for_min_value(hash)
   smallest_key = nil
@@ -20,5 +20,3 @@ def key_for_min_value(hash)
   end
   smallest_key
 end
-
-key_for_min_value(ikea)
